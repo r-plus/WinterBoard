0a1d561aa89b97e23575e69022b6ef4b42bf0400
Jay Freeman (saurik) <saurik@saurik.com>
2009-08-15 08:00:48 +0000

    Fixed nlist() UIImages on Thumb.

--- UIImages.mm
+++ UIImages.mm
@@ -10,18 +10,34 @@ extern "C" {
 
 extern "C" NSData *UIImagePNGRepresentation(UIImage *image);
 
+template <typename Type_>
+static void nlset(Type_ &function, struct nlist *nl, size_t index) {
+    struct nlist &name(nl[index]);
+    uintptr_t value(name.n_value);
+    if ((name.n_desc & N_ARM_THUMB_DEF) != 0)
+        value |= 0x00000001;
+    function = reinterpret_cast<Type_>(value);
+}
+
 int main(int argc, char *argv[]) {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
     struct nlist nl[4];
+
     memset(nl, 0, sizeof(nl));
     nl[0].n_un.n_name = (char *) "___mappedImages";
     nl[1].n_un.n_name = (char *) "__UISharedImageInitialize";
     nl[2].n_un.n_name = (char *) "_LoadMappedImageRef";
     nlist("/System/Library/Frameworks/UIKit.framework/UIKit", nl);
-    NSMutableDictionary **images = (id *) nl[0].n_value;
-    void (*__UISharedImageInitialize)(bool) = (void (*)(bool)) nl[1].n_value;
-    CGImageRef (*_LoadMappedImageRef)(CFStringRef) = (CGImageRef (*)(CFStringRef)) nl[2].n_value;
+
+    NSMutableDictionary **images;
+    nlset(images, nl, 0);
+
+    void (*__UISharedImageInitialize)(bool);
+    nlset(__UISharedImageInitialize, nl, 1);
+
+    CGImageRef (*_LoadMappedImageRef)(CFStringRef);
+    nlset(_LoadMappedImageRef, nl, 2);
 
     __UISharedImageInitialize(false);
 
